106305fdc6d1dff847df63cc4a0ed219359b91ae
Haziq Jamil <h.jamil@lse.ac.uk>
2018-12-18 15:22:21 +0800

    Updated print method for diagaccSim1

--- R/print.R
+++ R/print.R
@@ -1,66 +1,12 @@
-sim_res <- function(res, part.of.sim = TRUE) {
-  if (isTRUE(part.of.sim)) {
-    p <- getOption("diagacc.p")
-    item.names <- getOption("diagacc.item.names")[1:p]
-  } else {
-    item.names <- rownames(res[[1]]$sens.and.spec)
-    p <- length(item.names)
-  }
-
-  prev.vec <- sapply(res, function(x) x$prevalence)
-  mean.prev <- mean(prev.vec)
-  sd.prev <- sd(prev.vec)
-
-  se.prev.vec <- sapply(res, function(x) x$se.prev)
-  se.mean.prev <- mean(se.prev.vec)
-  se.sd.prev <- sd(se.prev.vec)
-
-  sens.and.spec.array <- array(
-    unlist(lapply(res, function(x) x$sens.and.spec[1:p, ])), dim = c(p, 2, 2)
-  )
-  mean.sens.and.spec <- apply(sens.and.spec.array, c(1, 2), mean)
-  sd.sens.and.spec <- apply(sens.and.spec.array, c(1, 2), sd)
-
-  se.sens.and.spec.array <- array(
-    unlist(lapply(res, function(x) x$se.sens.and.spec)), dim = c(p, 2, 2)
-  )
-  se.mean.sens.and.spec <- apply(se.sens.and.spec.array, c(1, 2), mean)
-  se.sd.sens.and.spec <- apply(se.sens.and.spec.array, c(1, 2), sd)
-
-  tab.est <- data.frame(cbind(
-    Mean = c(mean.prev, c(mean.sens.and.spec)),
-    SE   = c(sd.prev, c(sd.sens.and.spec))
-  ))
-  tab.est <- cbind(Est = tab.est[, 1], "2.5%" = tab.est$Mean - 1.96 * tab.est$SE,
-                   "97.5%" = tab.est$Mean + 1.96 * tab.est$SE)
-
-  tab.se <- data.frame(cbind(
-    Mean = c(se.mean.prev, c(se.mean.sens.and.spec)),
-    SE   = c(se.sd.prev, c(se.sd.sens.and.spec))
-  ))
-  tab.se <- cbind(SE = tab.se[, 1], "2.5%" = tab.se$Mean - 1.96 * tab.se$SE,
-                  "97.5%" = tab.se$Mean + 1.96 * tab.se$SE)
-
-  if (isTRUE(part.of.sim)) {
-    tab <- cbind(tab.est, tab.se)
-  } else {
-    tab <- data.frame(cbind(
-      Mean = c(mean.prev, c(mean.sens.and.spec)),
-      SE   = c(se.mean.prev, c(se.mean.sens.and.spec))
-    ))
-    tab$`2.5%` <- tab$Mean - 1.96 * tab$SE
-    tab$`97.5%` <- tab$Mean + 1.96 * tab$SE
-  }
-
-  rownames(tab) <- c("Prevalence", paste0("Sens.", item.names),
-                     paste0("Spec.", item.names))
-  tab
-
-}
-
 sim_tab_hat <- function(res, est.or.sd = c("est", "sd")) {
-  # The function to tabulate parameter replicates of run_sim() per $LC, $LCRE
-  # and $FM
+  # Helper function to tabulate parameter replicates of run_sim() per $LC, $LCRE
+  # and $FM.
+  #
+  # Args: A diagaccSim1 object. Also choose between parameter estimates or their
+  # post. SD.
+  #
+  # Returns: A (2p + 1) x B matrix, i.e. estimates along the rows, and
+  # replicates along the columns.
 
   est.or.sd <- match.arg(est.or.sd, c("est", "sd"))
   item.names <- rownames(res[[1]]$sens.and.spec)
@@ -87,6 +33,84 @@ sim_tab_hat <- function(res, est.or.sd = c("est", "sd")) {
   )
 
   tab.hat
+}
+
+
+#' Helper function to provide summary of simulation results
+#'
+#' This is an internal helper function to provide summary of simulation results.
+#'
+#' @param x A \code{diagaccSim1} object.
+#' @param type Choose between \code{"est"}, \code{"bias"} or \code{"sd"}.
+#' @param raw (Logical) Provide the raw matrix of replicates?
+#'
+#' @export
+sim_res <- function(x, type = c("est", "bias", "sd"), raw = FALSE) {
+  if (extract_miss.prop(x) == 1) {
+    truth <- true_vals(x)
+  } else {
+    truth <- true_vals_with_gs(x)
+  }
+  type <- match.arg(type, c("est", "bias", "sd"))
+  res <- list(
+    LC = sim_tab_hat(x$LC, ifelse(type == "sd", "sd", "est")),
+    LCRE = sim_tab_hat(x$LCRE, ifelse(type == "sd", "sd", "est")),
+    FM = sim_tab_hat(x$FM, ifelse(type == "sd", "sd", "est"))
+  )
+  if (type == "bias") {
+    res <- lapply(res, function(z) z - truth)
+  }
+
+  if (isTRUE(raw)) {
+    return(res)
+  } else {
+    MEAN <- lapply(res, function(x) apply(x, 1, mean))
+    SE <-  lapply(res, function(x) apply(x, 1, sd))
+    res <- list(
+      LC   = t(mapply(MEAN$LC, SE$LC, FUN = cbind)),
+      LCRE = t(mapply(MEAN$LCRE, SE$LCRE, FUN = cbind)),
+      FM   = t(mapply(MEAN$FM, SE$FM, FUN = cbind))
+    )
+    UL <- lapply(res, function(z) z[, 1] + qnorm(0.975) * z[, 2])
+    LL <- lapply(res, function(z) z[, 1] - qnorm(0.975) * z[, 2])
+    res <- mapply(res, LL, UL, FUN = cbind, SIMPLIFY = FALSE)
+
+    colnames(res$LC) <- colnames(res$LCRE) <- colnames(res$FM) <- c(
+      toupper(type), "SE", "2.5%", "97.5%"
+    )
+    res
+  }
+}
+
+#' @export
+print.diagaccSim1 <- function(x, type = c("est", "bias", "sd"), digits = 5, ...) {
+  cat("Summary from", x$sim.settings$B, "replications (")
+  sim.msg <- x$sim.settings$sim.msg
+  cat(gsub("\\n", ")\n", sim.msg))
+  cat("Statistic: ")
+  type <- match.arg(type, c("est", "bias", "sd"))
+  if (type == "est") type.print <- "Parameter estimates"
+  if (type == "bias") type.print <- "Parameter bias"
+  if (type == "sd") type.print <- "Posterior SD of parameters"
+  cat(type.print, "\n\n")
+
+  res <- sim_res(x, type)
+  cat("LC model fit\n")
+  print(round(res$LC, digits))
+  cat("\nLCRE model fit\n")
+  print(round(res$LCRE, digits))
+  cat("\nFM model fit\n")
+  print(round(res$FM, digits))
+}
+
+#' @export
+print.diagaccSim2 <- function(x, sim.key, ...) {
+  if (missing(sim.key)) {
+    cat("Use print(object, sim.key = <number>), where <number> is from the following:\n")
+    cat(paste0(utils::capture.output(x$sim.key), collapse = "\n"))
+  } else {
+    print(x[[sim.key]])
+  }
 }
 
 sim_res2 <- function(x, raw = FALSE) {
@@ -124,60 +148,5 @@ sim_res2 <- function(x, raw = FALSE) {
       "EST", "BIAS", "MSE", "SD"
     )
     return(res)
-  }
-}
-
-#' @export
-print.diagaccSim1 <- function(x, raw = FALSE, ...) {
-  if (isTRUE(raw)) {
-    sim_res2(x, raw = TRUE)
-  } else {
-    res <- sim_res2(x, raw = FALSE)
-    cat("LC model fit\n")
-    print(res$LC)
-    cat("\nLCRE model fit\n")
-    print(res$LCRE)
-    cat("\nFM model fit\n")
-    print(res$FM)
-  }
-}
-
-# print.diagaccSim1 <- function(x, ...) {
-#   cat("LC model fit\n")
-#   tmp <- cbind(truth = true_vals(x), sim_res(x$LC))
-#   tmp <- paste0(utils::capture.output(iprior::dec_plac(tmp, 3)), collapse = "\n")
-#   tmp <- gsub('"', " ", tmp)
-#   tmp <- gsub("truth    Est      2.5%     97.5%    SE       2.5%     97.5%",
-#               "  truth    Est      2.5%     97.5%    SE       2.5%     97.5%",
-#               tmp)
-#   cat(gsub('"', " ", tmp))
-#
-#   cat("\n\nLCRE model fit\n")
-#   tmp <- cbind(truth = true_vals(x), sim_res(x$LCRE))
-#   tmp <- paste0(utils::capture.output(iprior::dec_plac(tmp, 3)), collapse = "\n")
-#   tmp <- gsub('"', " ", tmp)
-#   tmp <- gsub("truth    Est      2.5%     97.5%    SE       2.5%     97.5%",
-#               "  truth    Est      2.5%     97.5%    SE       2.5%     97.5%",
-#               tmp)
-#   cat(gsub('"', " ", tmp))
-#
-#   cat("\n\nFM model fit\n")
-#   tmp <- cbind(truth = true_vals(x), sim_res(x$FM))
-#   tmp <- paste0(utils::capture.output(iprior::dec_plac(tmp, 3)), collapse = "\n")
-#   tmp <- gsub('"', " ", tmp)
-#   tmp <- gsub("truth    Est      2.5%     97.5%    SE       2.5%     97.5%",
-#               "  truth    Est      2.5%     97.5%    SE       2.5%     97.5%",
-#               tmp)
-#   cat(gsub('"', " ", tmp))
-#   cat("\n")
-# }
-
-#' @export
-print.diagaccSim2 <- function(x, sim.key, ...) {
-  if (missing(sim.key)) {
-    cat("Use print(object, sim.key = <number>), where <number> is from the following:\n")
-    cat(paste0(utils::capture.output(x$sim.key), collapse = "\n"))
-  } else {
-    print(x[[sim.key]])
   }
 }
