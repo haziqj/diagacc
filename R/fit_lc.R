ca880eec51da90d100af45ffac980c6777971d09
Haziq Jamil <h.jamil@lse.ac.uk>
2019-01-22 11:36:19 +0800

    Re-added deviance to fit

--- R/fit_lc.R
+++ R/fit_lc.R
@@ -117,7 +117,7 @@ fit_lc_mcmc <- function(X, n.sample = 2000, n.chains = 1, n.thin = 1,
     }
 
     #data# X, n, p
-    #monitor# tau, sens, spec
+    #monitor# tau, sens, spec, deviance
     "
   } else {
     # This is the model for NO gold standard -----------------------------------
@@ -143,7 +143,7 @@ fit_lc_mcmc <- function(X, n.sample = 2000, n.chains = 1, n.thin = 1,
       }
     }
     #data# X, n, p
-    #monitor# tau, sens, spec
+    #monitor# tau, sens, spec, deviance
     "
   }
 
